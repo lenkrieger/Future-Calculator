34c735376244ee286c14a78bd7b1c9ddfc1f66d2
roma615 <45481996+roma615@users.noreply.github.com>
2019-02-26 22:08:06 +0100

    так буде краще
    дабавте меню

--- main.cpp
+++ main.cpp
@@ -7,23 +7,18 @@ class A1{
      float a1;
      float a2;
     
-     void tail(){
+    void tail(){
         cout << "\nВведите два числа: ";
         cin >> a1 >> a2;
         }; 
-        
-     virtual void paw(){
-         };
-        
+    
 };
-
-
 
 class Dodavanie : public A1{
     public:
      float s;
-     virtual void paw(){
-        cout << "\n-----Вы выбрали сумму-----";
+     void summa(){
+        cout << "-----Вы выбрали сумму-----";
         tail();
         s = a1 + a2;
         cout << "\n-----Ответ: " << s << " -----";
