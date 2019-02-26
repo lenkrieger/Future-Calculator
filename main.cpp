a75fe766d431ae7951c09e21673436557980a6b6
roma615 <45481996+roma615@users.noreply.github.com>
2019-02-26 21:57:18 +0100

    перший варянт))))

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+
+    cout << "podaj liczby:" << endl;
+    cin >> a;
+    cout << "podaj drugu liczbu:" << endl;
+    cin >> b;
+    sum = a + b;
+    cout << "suma = " << sum << endl;
+
+
+    return 0;
+}
